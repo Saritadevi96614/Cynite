81d85db8b37da498ee849f615b113776cc4b7b59
Brarharman238 <106009179+Brarharman238@users.noreply.github.com>
2022-05-22 13:40:16 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -47,7 +47,7 @@ _𝐂𝐨𝐧𝐭𝐚𝐜𝐭 𝐌𝐞 𝐅𝐨𝐫 𝐊𝐧𝐨𝐰 𝐀𝐛�
 ➾ /delall - <code>delete the whole filters in a chat (chat owner only)</code>"""
     BUTTON_TXT = """Help: <b>Buttons</b>
 
-- ᗩᒍᗩ᙭ Supports both url and alert inline buttons.
+- Cynite Supports both url and alert inline buttons.
 
 <b>NOTE:</b>
 1. Telegram will not allows you to send buttons without any content, so content is mandatory.
